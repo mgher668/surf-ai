afd2d314bc7e2a50b16be521fd1e169d05cd1886
mgher <mgher668@protonmail.com>
2026-05-31 23:26:09 +0800

    feat(extension): add app icons and brand assets

--- apps/extension/src/manifest.ts
+++ apps/extension/src/manifest.ts
@@ -7,7 +7,19 @@ export default defineManifest({
   version: "0.1.0",
   default_locale: "en",
   action: {
-    default_popup: "src/ui/popup/index.html"
+    default_popup: "src/ui/popup/index.html",
+    default_icon: {
+      "16": "icons/icon-16.png",
+      "32": "icons/icon-32.png",
+      "48": "icons/icon-48.png",
+      "128": "icons/icon-128.png"
+    }
+  },
+  icons: {
+    "16": "icons/icon-16.png",
+    "32": "icons/icon-32.png",
+    "48": "icons/icon-48.png",
+    "128": "icons/icon-128.png"
   },
   options_page: "src/ui/settings/index.html",
   background: {
